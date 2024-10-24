fd92e3d91e1d064f3549648f286130e850257a5f
aishwarya <aishwaryasardey24@gmail.com>
2024-10-24 17:55:14 +0530

    EntityClass-Aish

--- TicketManangement2/src/main/java/com/example/demo/model/Project.java
+++ TicketManangement2/src/main/java/com/example/demo/model/Project.java
@@ -1,11 +1,40 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import lombok.Data;
+import java.time.LocalDate;
+import java.util.List;
 
-@Entity
-@Data
-public class Project 
-{
-	///hiiiiiiiii
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+   @Entity
+   @Table(name="ProjectTable")
+   @NoArgsConstructor
+   @Data
+    public class Project {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer projectId;
+	@NonNull
+	private String projectName;
+	@NonNull
+	private String projectDescription;
+	@NonNull
+	private LocalDate creationDate;
+	@NonNull
+	private Boolean isProjectAvailable=true;
+	
+	@OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+	private List<Employee> employees;
+
+	@OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+	private List<Ticket> tickets;
+
 }
